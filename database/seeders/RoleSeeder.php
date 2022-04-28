0260e5649925ae6ea51dc610f39beb010659137a
Andreas Asatera <tolong@buatin.website>
2022-04-28 10:05:41 +0700

    tambah seeder

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    public function run()
+    {
+        $roles = [
+            'admin',
+            'user'
+        ];
+
+        foreach ($roles as $role) {
+            Role::firstOrCreate([
+                'name' => $role
+            ]);
+        }
+    }
+}
